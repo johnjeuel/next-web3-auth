b80488c44ebf62a73813ada95f46fe0cb166a0d2
Jeuel Joaquin <jeuel.joaquin13@gmail.com>
2022-05-22 22:58:54 +0800

    template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {MoralisProvider} from 'react-moralis'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <MoralisProvider
+  appId="8UqxhvTS0blFU4C1BvKLYglCmYA7ZVYPf9ORDMJN"
+  serverUrl="https://pylv9wpqdww2.usemoralis.com:2053/server">
+    <Component {...pageProps} />
+  </MoralisProvider>
 }
 
 export default MyApp
